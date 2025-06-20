e308d6cfd4a69755d13b1881545c95ae8cbed302
Ever Ronaldo <ever.condori@upeu.edu.pe>
2025-06-20 11:07:16 -0500

    Actualizar estado de autenticación inicial en AuthProvider: establecer isAuthenticated en true

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -129,7 +129,7 @@ const authAPI = {
 };
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
