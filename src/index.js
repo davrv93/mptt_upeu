91a6dc3bbd7feb9a277eadcdb13d68a005763d68
Ever Ronaldo <ever.condori@upeu.edu.pe>
2025-06-23 10:57:57 -0500

    Actualizar la ruta de importación del componente App en index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import App from './app/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
