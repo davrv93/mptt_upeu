061407aeadb17d36e36e2252379b34195bc7fba7
Ever Ronaldo <ever.condori@upeu.edu.pe>
2025-06-23 11:05:05 -0500

    Agregar archivo de índice para exportar componentes y hooks del módulo syllabus

---
+++ src/features/syllabus/index.js
@@ -0,0 +1,17 @@
+// Syllabus feature exports
+export { default as SyllabusEditor } from './components/SyllabusEditor';
+export { default as SyllabusHeader } from './components/SyllabusHeader';
+export { default as SyllabusSidebar } from './components/SyllabusSidebar';
+export { default as SyllabusContent } from './components/SyllabusContent';
+export { default as SectionCard } from './components/SectionCard';
+export { default as FormField } from './components/FormField';
+export { default as ToastNotification } from './components/ToastNotification';
+
+// Hooks
+export { useSyllabusData } from './hooks/useSyllabusData';
+export { useSyllabusValidation } from './hooks/useSyllabusValidation';
+export { usePDFGeneration } from './hooks/usePDFGeneration';
+export { useSyllabusUI } from './hooks/useSyllabusUI';
+
+// Utils
+export * from './utils/syllabusUtils';
