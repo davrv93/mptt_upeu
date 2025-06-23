231f13739883937a96e0efebeb3cc70a30805e29
Ever Ronaldo <ever.condori@upeu.edu.pe>
2025-06-23 14:05:51 -0500

    Eliminar comentarios innecesarios en useTemplateState para mejorar la legibilidad del código

--- src/features/template/hooks/useTemplateState.js
+++ src/features/template/hooks/useTemplateState.js
@@ -58,6 +58,3 @@ export const useTemplateState = () => {
 
   return { templateExists, templateStats, triggerTemplateUpdate };
 };
-
-
-// ara reducir codigo del componente TemplateBuilder y también los styles  y en lo visual haz mejorad para que el diseño sea un diseño intuitivo y claro que permite a los usuarios encontrar lo que necesitan rápidamente y sin complicaciones y que en '**Agregar Nueva Sección**' los **Campo **se puedan mover para arriba y abajo y también creo que sería bueno separa los modals en otro componente para refacturizar el codigo y reducir las linas de codigo esto como un programador senior y esto debuelme en archivos separados y sin el test
