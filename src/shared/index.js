2809b2fe40edfb7a9fc7dce36a875780877ba3c6
Ever Ronaldo <ever.condori@upeu.edu.pe>
2025-06-23 11:07:23 -0500

    Agregar archivo index.js para exportaciones compartidas de componentes, hooks, utilidades y configuración

---
+++ src/shared/index.js
@@ -0,0 +1,11 @@
+// Shared exports
+export { default as Home } from './components/layout/Home';
+
+// Hooks
+export { useDragAndDrop } from './hooks/useDragAndDrop';
+
+// Utils
+export * from './utils/pdfUtils';
+
+// Config
+export { environment } from './config/environment';
