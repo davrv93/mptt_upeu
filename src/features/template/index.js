3f2ea15aa1d34f5a198344b7efbc759510abbf90
Ever Ronaldo <ever.condori@upeu.edu.pe>
2025-06-23 11:06:20 -0500

    Agregar exportaciones para la funcionalidad de plantilla, incluyendo componentes y hooks

---
+++ src/features/template/index.js
@@ -0,0 +1,8 @@
+// Template feature exports
+export { default as TemplateBuilder } from './components/TemplateBuilder';
+export { default as SimpleFieldsConfigurator } from './components/SimpleFieldsConfigurator';
+
+// Hooks
+export { useMPTTNodes } from './hooks/useMPTTNodes';
+export { useAttributesDragDrop } from './hooks/useAttributesDragDrop';
+export { useTemplateState } from './hooks/useTemplateState';
